d243aa0a6aecb3fa705a67716dfbdc6d26bfea7f
alexvorxx <alexvorxx@mail.ru>
2025-02-17 21:13:16 +0300

    Fixed a problem with ALSA where the sound was turned off when connecting/disconnecting headphones.

--- app/src/main/java/com/winlator/alsaserver/ALSAClient.java
+++ app/src/main/java/com/winlator/alsaserver/ALSAClient.java
@@ -1,5 +1,8 @@
 package com.winlator.alsaserver;
 
+import android.media.AudioFormat;
+import android.media.AudioTrack;
+
 import com.winlator.sysvshm.SysVSharedMemory;
 
 import java.nio.ByteBuffer;
@@ -18,14 +21,58 @@ public class ALSAClient {
     private byte channelCount = 2;
     private int sampleRate = 0;
     private int position;
+    private short previousUnderrunCount = 0;
+    private ByteBuffer auxBuffer;
+    private int bufferCapacity;
     private int bufferSize;
+    private byte channels = 2;
     private int frameBytes;
     private ByteBuffer sharedBuffer;
-    private boolean playing = false;
-    private long streamPtr = 0;
+    public float volume = 1.0F;
+    private static short framesPerBuffer = 256;
+
+    private AudioTrack track = null;
 
     static {
         System.loadLibrary("winlator");
+    }
+
+    public static int getPCMEncoding(DataType paramDataType) {
+        switch (paramDataType) {
+            default:
+                return 1;
+            case FLOATLE:
+            case FLOATBE:
+                return 4;
+            case S16LE:
+            case S16BE:
+                return 2;
+            case U8:
+                break;
+        }
+        return 3;
+    }
+
+    private void increaseBufferSizeIfUnderrunOccurs() {
+        int i = this.track.getUnderrunCount();
+        if (i > this.previousUnderrunCount) {
+            int j = this.bufferSize;
+            if (j < this.bufferCapacity) {
+                this.previousUnderrunCount = (short)i;
+                j += framesPerBuffer;
+                this.bufferSize = j;
+                this.track.setBufferSizeInFrames(j);
+            }
+        }
+    }
+
+    public static int getChannelConfig(int paramInt) {
+        if (paramInt <= 1) {
+            paramInt = 4;
+        } else {
+            paramInt = 12;
+        }
+        return paramInt;
     }
 
     public void release() {
@@ -34,46 +81,56 @@ public class ALSAClient {
             sharedBuffer = null;
         }
 
-        stop(streamPtr);
-        close(streamPtr);
-        playing = false;
-        streamPtr = 0;
+        AudioTrack track1 = this.track;
+        if (track1 != null) {
+            track1.pause();
+            this.track.flush();
+            this.track.release();
+            this.track = null;
+        }
     }
 
     public void prepare() {
         position = 0;
+        previousUnderrunCount = 0;
         frameBytes = channelCount * dataType.byteCount;
         release();
 
         if (!isValidBufferSize()) return;
 
-        streamPtr = create(dataType.ordinal(), channelCount, sampleRate, bufferSize);
-        if (streamPtr > 0) start();
+        AudioFormat format = (new AudioFormat.Builder()).setEncoding(getPCMEncoding(this.dataType)).setSampleRate(this.sampleRate).setChannelMask(getChannelConfig(this.channels)).build();
+        AudioTrack track1 = (new AudioTrack.Builder()).setPerformanceMode(AudioTrack.PERFORMANCE_MODE_LOW_LATENCY).setAudioFormat(format).setBufferSizeInBytes(getBufferSizeInBytes()).build();
+        this.track = track1;
+        this.bufferCapacity = track1.getBufferCapacityInFrames();
+
+        this.track.setVolume(volume);
+        this.track.play();
     }
 
     public void start() {
-        if (streamPtr > 0 && !playing) {
-            start(streamPtr);
-            playing = true;
-        }
+        AudioTrack track1 = this.track;
+        if (track1 != null && track1.getPlayState() != 3)
+            this.track.play();
     }
 
     public void stop() {
-        if (streamPtr > 0 && playing) {
-            stop(streamPtr);
-            playing = false;
+        AudioTrack track1 = this.track;
+        if (track1 != null) {
+            track1.stop();
+            this.track.flush();
         }
     }
 
     public void pause() {
-        if (streamPtr > 0) {
-            pause(streamPtr);
-            playing = false;
-        }
+        AudioTrack track1 = this.track;
+        if (track1 != null)
+            track1.pause();
     }
 
     public void drain() {
-        if (streamPtr > 0) flush(streamPtr);
+        AudioTrack track1 = this.track;
+        if (track1 != null)
+            track1.flush();
     }
 
     public void writeDataToStream(ByteBuffer data) {
@@ -84,16 +141,27 @@ public class ALSAClient {
             data.order(ByteOrder.BIG_ENDIAN);
         }
 
-        if (playing) {
-            int numFrames = data.limit() / frameBytes;
-            int framesWritten = write(streamPtr, data, numFrames);
-            if (framesWritten > 0) position += framesWritten;
+        if (this.track != null) {
+            data.position(0);
+            try {
+                while (this.track.write(data, data.remaining(), AudioTrack.WRITE_BLOCKING) >= 0) {
+                    increaseBufferSizeIfUnderrunOccurs();
+                    if (data.position() == data.limit())
+                        break;
+                }
+            } catch (Exception exception) {}
+            this.position += data.position();
             data.rewind();
         }
     }
 
     public int pointer() {
-        return position;
+        int value;
+        if (this.track != null)
+            value = this.position / this.frameBytes;
+        else
+            value = 0;
+        return value;
     }
 
     public void setDataType(DataType dataType) {
@@ -117,7 +185,15 @@ public class ALSAClient {
     }
 
     public void setSharedBuffer(ByteBuffer sharedBuffer) {
-        this.sharedBuffer = sharedBuffer;
+        if (sharedBuffer != null) {
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(getBufferSizeInBytes());
+            ByteOrder byteOrder = ByteOrder.LITTLE_ENDIAN;
+            this.auxBuffer = byteBuffer.order(byteOrder);
+            this.sharedBuffer = sharedBuffer.order(byteOrder);
+        } else {
+            this.auxBuffer = null;
+            this.sharedBuffer = null;
+        }
     }
 
     public DataType getDataType() {
