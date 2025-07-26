df5e97d33e2ea4020cc39c8a800d9c105a02a6ca
TGP-17 <mytablet19048@gmail.com>
2025-07-26 23:33:49 +0200

    Add DXVK_HUD_OPACITY AND DXVK_HUD_SCALE

--- app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
+++ app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
@@ -34,7 +34,9 @@ public class EnvVarsView extends FrameLayout {
         {"mesa_glthread", "CHECKBOX", "false", "true"},
         {"WINEESYNC", "CHECKBOX", "0", "1"},
         {"TU_DEBUG", "SELECT_MULTIPLE", "startup", "nir", "nobin", "sysmem", "gmem", "forcebin", "layout", "noubwc", "nomultipos", "nolrz", "nolrzfc", "perf", "perfc", "flushall", "syncdraw", "push_consts_per_stage", "rast_order", "unaligned_store", "log_skip_gmem_ops", "dynamic", "bos", "3d_load", "fdm", "noconform", "rd"},
-        {"DXVK_HUD", "SELECT_MULTIPLE", "devinfo", "fps", "frametimes", "submissions", "drawcalls", "pipelines", "descriptors", "memory", "gpuload", "version", "api", "cs", "compiler", "samplers"},
+        {"DXVK_HUD_SCALE", "NUMBER"},
+        {"DXVK_HUD_OPACITY", "NUMBER"},
+        {"DXVK_HUD", "SELECT_MULTIPLE", "devinfo", "fps", "frametimes", "submissions", "drawcalls", "pipelines", "descriptors", "memory", "gpuload", "version", "api", "cs", "compiler", "samplers", "scale=$DXVK_HUD_SCALE", "opacity=$DXVK_HUD_OPACITY"},
         {"MESA_EXTENSION_MAX_YEAR", "NUMBER"},
         {"PULSE_LATENCY_MSEC", "NUMBER"},
         {"MANGOHUD", "CHECKBOX", "0", "1"}
